cd0604826347d2624a514306f71470fb5825e0af
Angel Panduro <84420635+Angel-Jesus@users.noreply.github.com>
2022-05-24 23:16:32 -0500

    secret

--- ESP32_FIrebase_HiWasi/src/secret.h
+++ ESP32_FIrebase_HiWasi/src/secret.h
@@ -1,11 +1,11 @@
 //Secret to WIFI
-const char* WIFI_SSID = "EDGAR";
-const char* WIFI_PASSWORD = "edgpan21";
+const char* WIFI_SSID = "**************";
+const char* WIFI_PASSWORD = "**************";
 //Secret to Firebase Real Time DataBase
-const char* API_KEY = "AIzaSyDp_UcbCNRsMVg4SAGCg78g_uBeJ-RMlB4";
-const char*  DATABASE_URL = "hiwasi2-default-rtdb.firebaseio.com"; //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
-const char* USER_EMAIL = "panduro1721@gmail.com";
-const char* USER_PASSWORD = "papaya11";
+const char* API_KEY = "**************";
+const char*  DATABASE_URL = "**************"; //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+const char* USER_EMAIL = "**********************";
+const char* USER_PASSWORD = "*******";
 
 //Secret to Firebase Cloud Message
-const char* FIREBASE_FCM_SERVER_KEY = "AAAA2e37JkU:APA91bELvRPVwpGB7txmB7V9SirOtDWLCbCBJiZg3qvWxaX6W1Sy2Z87EWxA99KlsyoO6Vh84eJ2nPZ7kiioHlrP6K3ag9t_HJxnpQ9Ho_14-OP9jlHEfwYROSwwHMrVnBOo0nrDVJgw";
+const char* FIREBASE_FCM_SERVER_KEY = "**************************";
